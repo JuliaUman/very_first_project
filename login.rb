d3b9406578ccef97ee224ff85f9093850ea6ea33
JuliaUman <julia_uman@yahoo.com>
2015-11-29 22:45:02 +0200

    login and logout functions

---
+++ login.rb
@@ -0,0 +1,14 @@
+require 'selenium-webdriver'
+
+browser = Selenium::WebDriver.for :firefox
+
+browser.get 'http://demo.redmine.org'
+
+browser.find_element(:class, 'login').click
+browser.find_element(:id, 'username').send_keys 'Flower'
+browser.find_element(:id, 'password').send_keys 'Qwerty123'
+
+
+browser.find_element(:name, 'login').click
+
+browser.find_element(:class, 'logout').click
